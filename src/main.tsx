434735dde7424f2edc561ee50fa33339c969dd57
SM Riyadh <sm.hussain@bkash.com>
2024-11-20 00:49:18 +0600

    Add Scaffold.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './style.css'
+import Scaffold from "./pages/Scaffold.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Scaffold />
   </StrictMode>,
 )
